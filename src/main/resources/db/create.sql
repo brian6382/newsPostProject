4a5e261ae21404ca560deb6487225f1f4cd5a787
brian6382 <briankulei98@gmail.com>
2021-08-23 12:46:49 +0300

    add table users

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,4 +1,4 @@
-create DATABASE newsportal;
+create dATABASE newsportal;
 CREATE TABLE users (id serial PRIMARY KEY, name varchar, position varchar, role varchar, departmentId int);
 CREATE TABLE departments (id serial PRIMARY KEY, name varchar, description varchar);
 CREATE TABLE news (id serial PRIMARY KEY, userId int, content varchar, postdate timestamp, departmentId int, type varchar);
